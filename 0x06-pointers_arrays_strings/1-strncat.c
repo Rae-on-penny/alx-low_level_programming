477365670a08686017eceae7b7354b95ee065a7d
Rae-on-penny <oyedaresunday15@gmail.com>
2022-10-28 15:47:56 -0700

    new

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,30 @@
 #include "main.h"
 
 /**
- * -strncat - concatenate two strings
- *  @dest: copy to 
+ * _strncat - joins two strings with n bytes
+ *  @dest: copy to
  *  @src: copy from
- *  Return: pointer to dest
+ *  @n: number of character to be copied
+ *  Return: dest
  */
-char *_strncatwq
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
 
